03823baa58c5f20f2e1ead9676fec3f4cf3965fb
gabymartz <nmartineza2@est.ups.edu.ec>
2024-08-12 08:55:20 -0500

    Add(I): Add Book and Author

---
+++ src/main/java/com/ups/oop/service/BookService.java
@@ -0,0 +1,13 @@
+package com.ups.oop.service;
+
+
+import com.ups.oop.dto.BookDTO;
+import com.ups.oop.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+}
